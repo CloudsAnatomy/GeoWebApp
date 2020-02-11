937c2f22461c4670eb075b7f07227b23656657c7
Talal Ali <talal.ali@systemsltd.com>
2020-02-12 02:51:20 +0500

    Add project files.

---
+++ README.md
@@ -0,0 +1,10 @@
+# GeoWebApp
+This is an example of geo-distributed web application using Azure Traffic Manager. 
+
+## Links
+
+1. step by step tutorial in [my blog]
+2. check out [live demo]
+
+[my blog]: http://backend-devs.com/blog/archive/multi-regional-aspnet-core-with-azure-traffic-manager/
+[live demo]: http://geowebapp2.trafficmanager.net/
---
+++ src/GeoWebApp/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GeoWebApp.Models;
+using Microsoft.Extensions.Options;
+
+namespace GeoWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        protected readonly AppSettings _appSettings;
+
+        public HomeController(IOptions<AppSettings> appSettings)
+        {
+            _appSettings = appSettings.Value;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.DataCenter = Environment.GetEnvironmentVariable("DATA_CENTER") ?? "Unknown";
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
